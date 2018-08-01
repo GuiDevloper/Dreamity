b7d130961d3b594d43bdde27d5ae8011c3a4cf02
GuiDevloper <guicorrea384@gmail.com>
2018-07-31 21:31:54 -0300

    Added components imports to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
