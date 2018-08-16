dc4421d2fdf4267b968acb5a21d3b8bd3a8216f8
GuiDevloper <guicorrea384@gmail.com>
2018-08-15 22:05:21 -0300

    Added firebase && auth test

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  users: Observable<any>;
 
-  constructor() { }
+  constructor(
+    db: AngularFireDatabase,
+    public ngAuth: AngularFireAuth,
+    private ngZone: NgZone,
+    private router: Router
+  ) {
+    this.users = db.object('/users/1/login').valueChanges();
+  }
+  login() {
+    const git = new auth.GithubAuthProvider();
+    const rt = this.router;
+    const ngz = this.ngZone;
+    this.ngAuth.auth.signInWithPopup(git).then(function() {
+      ngz.run(() => {
+        rt.navigateByUrl('/');
+      });
+    });
+  }
+  logout() {
+    this.ngAuth.auth.signOut();
+  }
 
   ngOnInit() {
   }
