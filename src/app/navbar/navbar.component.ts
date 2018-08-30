a9c1cdf04cf3447eff2aca443a66663f64a200ef
GuiDevloper <guicorrea384@gmail.com>
2018-08-29 23:53:46 -0300

    Improved dynamic navbar using user data async

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './../user.service';
+import { share } from 'rxjs/operators';
+import { User } from '../user';
 
 @Component({
   selector: 'app-navbar',
@@ -6,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  navli = ['Home', 'About', 'Log in', 'Sign in'];
+  navli = [];
+  User: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(public user: UserService) {
+    this.isLogged();
+  }
+  // async SE user está logado
+  async isLogged() {
+    this.User = await this.user.userData();
+    let terceiro, quarto;
+    // armazena 3o e 4o
+    if (this.User) {
+      this.User.toPromise().then(function(val) {
+        terceiro = val.displayName;
+      });
+    }
+    quarto = this.User ? 'Log out' : 'Sign in';
+    // adiciona tudo a lista
+    this.navli = ['Home', 'About', terceiro, quarto];
+  }
 
   ngOnInit() {
   }
