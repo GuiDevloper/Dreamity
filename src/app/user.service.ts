bcb4d93b1a1c5e1de4f321e0ac0e75df43039c37
GuiDevloper <guicorrea384@gmail.com>
2018-08-31 23:20:31 -0300

    upd. userData/new logWith(provider)

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,29 +1,50 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { first } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 import { User } from './user';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user: User;
+  users: Observable<any>;
 
   constructor(public ngAuth: AngularFireAuth,
     private ngZone: NgZone,
-    private router: Router) { }
+    private router: Router, public db: AngularFireDatabase) {
+      this.users = db.object('/users/1/login').valueChanges();
+    }
 
-  userData(): Observable<firebase.User> {
-    return this.ngAuth.authState.pipe(first());
+  async userData() {
+    this.ngAuth.authState.subscribe(use => {
+      this.user = use;
+    });
+    return await this.user;
   }
 
-  Logout() {
+  Logout(): void {
     this.ngAuth.auth.signOut();
+    this.goTo('');
+  }
+
+  logWith(provider): void {
+    const rt = this.router;
+    const ngz = this.ngZone;
+    this.ngAuth.auth.signInWithPopup(provider).then((result) => {
+      this.goTo('');
+    }).catch((err) => {
+      this.logWith(provider);
+    });
+  }
+
+  goTo(url: string): void {
     const rt = this.router;
     const ngz = this.ngZone;
     ngz.run(() => {
-      rt.navigateByUrl('/');
+      rt.navigateByUrl(url);
     });
   }
 }
