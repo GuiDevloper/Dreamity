2a7bd1038b28c64c5c49a1aeb5968cd868b50f76
GuiDevloper <guicorrea384@gmail.com>
2018-08-31 23:22:33 -0300

    upd. model User compliant w/ userData

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,6 @@
 // Class pattern to user data
 export class User {
-  uid: number;
-  name: string;
-  username: string;
-  photo: string;
+  uid: string;
+  displayName: string;
+  email: string;
 }
