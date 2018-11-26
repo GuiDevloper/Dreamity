9638492261b3adecb1c6632ce147cb8f714f3c1b
GuiDevloper <guicorrea384@gmail.com>
2018-11-25 23:39:24 -0200

    Update get+set of dreams & params to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProfileComponent } from './profile/profile.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
   // {path: 'about', component: ProfileComponent},
-  {path: 'profile/:user', component: ProfileComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: ':user', component: ProfileComponent},
+  {path: ':profile/p/:dream', component: PostsComponent}
 ];
 
 @NgModule({
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,67 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class PostService {
   posts: any;
 
-  constructor(private db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase) { }
+
+  /*
+  * Puxa posts baseado no usuario armazenado
+  * @param usua: string = username || ''
+  **/
+  getPosts(usua): Observable<{}> {
+    return this.db.object(`posts/${usua}`).valueChanges();
   }
 
-  add(user, value) {
+  create(user, value) {
+    value = {
+      title: value[0],
+      text: value[1],
+      time: value[2]
+    };
     this.db.list(`/posts/${user}`).push(value);
   }
 
-  comment(path, value) {
-    this.db.list(`/posts/${path}`).push(value);
+  /*
+  * Transforma de timestamp para dd/mm/aaaa
+  * @param time: timestamp do post
+  **/
+  getDate(time): string {
+    const timePost = new Date(time);
+    // params de formatação
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    };
+    // formatado + tempo visual
+    return timePost.toLocaleDateString(undefined, options) +
+      ' ' + this.format(timePost);
   }
+
+  /*
+  * Formata time pra `Há ${tantos} min/horas/dias`
+  * @param timePost: new Date(timestamp)
+  **/
+  format(timePost): string {
+    // Obtem há quanto tempo foi postado
+    const tempodoPost = new Date().getTime() - timePost.getTime();
+    const minutos = this.n(tempodoPost / 60000);
+    const horas = this.n(minutos / 60);
+    const dias = this.n(horas / 24);
+    return dias ? `Há ${dias} dias.` :
+      (horas ? `Há ${horas} horas.` :
+      (minutos ? `Há ${minutos} minutos.` : 'Agora a pouco.'));
+  }
+
+  /*
+  * Normaliza pra inteiro caso fracionado
+  * @param time: min/hor/dia possivelmente
+  * fracionado após divisão
+  **/
+  n(time): number {
+    time += '';
+    // remove tudo após virgula
+    return time > 0 ? (time.substring(
+      0, time.indexOf('.')) || time) : false;
+  }
+
 }
