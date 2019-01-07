a970b20df041e2fa5c090487bf1f69c8f28e063c
GuiDevloper <guicorrea384@gmail.com>
2019-01-07 21:58:06 -0200

    cleaned Profile model use, changed editable profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { UserService } from '../core';
+import { Profile, UserService } from '../core';
 
 @Component({
   selector: 'app-profile',
@@ -9,38 +9,42 @@ import { UserService } from '../core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  usua: any = null;
-  profile: any = null;
+  profile: Profile;
   update = false;
   myProfile = false;
+  atualUsername: string;
 
   constructor(private user: UserService,
     private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.usua = this.route.snapshot.paramMap.get('user');
+    this.atualUsername = this.route.snapshot.paramMap.get('user');
+    // Obtem profile pela url
+    const pro = this.user.getProfile(this.atualUsername, 2);
+    pro[0].subscribe((prof: Profile) => {
+      this.setProfile(prof);
+    });
     this.user.isLogged().subscribe(use => {
       if (use) {
-        const u = this.user.getUser(use.uid);
-        if (!u[1]) {
-          // Armazena se este usuario está no profile
-          u[0].subscribe(user => this.myProfile = user === this.usua);
-        } else {
-          this.myProfile = u[0] === this.usua;
-        }
+        const u = this.user.getUser(use.uid, 2);
+        // Armazena se este usuario está no profile
+        u[0].subscribe((user: string) => {
+          this.setIsMy(user === this.atualUsername);
+        });
       }
     });
-    // Obtem profile pela url
-    const pro = this.user.getProfile(this.usua);
-    if (!pro[1]) {
-      pro[0].subscribe(prof => this.profile = prof);
-    } else {
-      this.profile = pro[0];
-    }
+  }
+
+  setIsMy(isMy: boolean): void {
+    this.myProfile = isMy;
+  }
+
+  setProfile(prof: Profile): void {
+    this.profile = prof;
   }
 
   editProfile() {
-    this.user.updateProfile(this.usua, this.profile);
+    this.user.updateProfile(this.profile, this.atualUsername);
     this.update = false;
   }
 
